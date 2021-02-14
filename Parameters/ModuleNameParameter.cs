d29f3db7f513e637f7ad7721b82a6b11848580e3
Jan Pernecky <daroiz@janper.sk>
2021-02-14 17:40:56 +0100

    Expose ModuleName floating parameter
    So that it is possible to use only Module name when creating lists of allowed Modules. It may require multiplication, which may be unefficient when using the full-fledged Modules.

--- Parameters/ModuleNameParameter.cs
+++ Parameters/ModuleNameParameter.cs
@@ -7,7 +7,7 @@ namespace Monoceros {
     /// Wraps <see cref="ModuleName"/> type so that it can be used in
     /// Grasshopper as an input, output or a floating parameter.
     /// </summary>
-    class ModuleNameParameter : GH_PersistentParam<ModuleName> {
+    public class ModuleNameParameter : GH_PersistentParam<ModuleName> {
         public ModuleNameParameter( ) : base("Module Name",
                                              "MN",
                                              "Contains a collection of Monoceros Module names.",
