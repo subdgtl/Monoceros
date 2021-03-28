f46c6ad76db128a6ba8b1f6ad0a3d3a3cfe818c6
janper <daroiz@janper.sk>
2021-03-28 22:13:33 +0200

    Trim now works

--- Components/TrimTest.cs
+++ Components/TrimTest.cs
@@ -5,6 +5,7 @@ using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
 using Rhino.DocObjects;
 using Rhino.Geometry;
+using Rhino.Geometry.Intersect;
 
 namespace Monoceros {
     public class ComponentTrimTest : GH_Component {
@@ -17,7 +18,7 @@ namespace Monoceros {
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager) {
             pManager.AddGeometryParameter("Geometry",
                                           "G",
-                                          "G",
+                                          "Geometry to be converted into Modules and Slots. (Point, Curve, Brep)",
                                           GH_ParamAccess.list);
         }
 
@@ -43,12 +44,38 @@ namespace Monoceros {
                 return;
             }
 
-            var geometryClean = geometryRaw
-                .Where(goo => goo != null)
-                .Select(ghGeo => {
-                    var geo = ghGeo.Duplicate();
-                    return GH_Convert.ToGeometryBase(geo);
-                }).ToList();
+            // Mesh geometry is not supported because the Rhino Mesh library
+            // lacks necessary features and shows unexpected (undocumented) behavior:
+            // - Mesh split does not work all the time
+            // - Mesh split causes excessive and unnecessary triangulation
+            // - Mesh split adds unnecessary vertices onto straight edges, splitting them
+            // - All above are inconsistent, causing different results where same results would be expected
+            // - Mesh Reduce that should simplify the mesh by joining faces that could become one, is erroneous and unpredictable
+
+            var geometryUnwrapped = geometryRaw
+                            .Where(goo => goo != null)
+                            .Select(ghGeo => {
+                                var geo = ghGeo.Duplicate();
+                                return GH_Convert.ToGeometryBase(geo);
+                            });
+
+            var geometryClean = new List<GeometryBase>();
+            var foundUnsupportedGeometry = false;
+
+            foreach (var geo in geometryUnwrapped) {
+                if (geo.ObjectType == ObjectType.Point
+                                || geo.ObjectType == ObjectType.Curve
+                                || geo.HasBrepForm) {
+                    geometryClean.Add(geo);
+                } else {
+                    foundUnsupportedGeometry = true;
+                }
+            }
+
+            if (foundUnsupportedGeometry) {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Warning,
+                    "Found unsupported geometry. Currently supports only Points, Curves and Breps.");
+            }
 
             if (!geometryClean.Any()) {
                 AddRuntimeMessage(GH_RuntimeMessageLevel.Error,
@@ -56,60 +83,105 @@ namespace Monoceros {
                 return;
             }
 
-            var geometryBoxes = geometryClean.Select(geometry => geometry.GetBoundingBox(true)).ToList();
-
             var unionBox = BoundingBox.Empty;
-            foreach (var geometryBox in geometryBoxes) {
-                unionBox.Union(geometryBox);
+            foreach (var geometry in geometryClean) {
+                unionBox.Union(geometry.GetBoundingBox(true));
             }
 
-            var outputGeometry = new List<GeometryBase>();
+            var minX = Math.Floor(unionBox.Min.X) - 1;
+            var maxX = Math.Ceiling(unionBox.Max.X) + 1;
+            var minY = Math.Floor(unionBox.Min.Y) - 1;
+            var maxY = Math.Ceiling(unionBox.Max.Y) + 1;
+            var minZ = Math.Floor(unionBox.Min.Z) - 1;
+            var maxZ = Math.Ceiling(unionBox.Max.Z) + 1;
+
+            var toBeChopped = new List<GeometryBase>(geometryClean);
+            var alreadyChopped = new List<GeometryBase>();
+
             for (var z = Math.Floor(unionBox.Min.Z); z < Math.Ceiling(unionBox.Max.Z); z++) {
-                for (var y = Math.Floor(unionBox.Min.Y); y < Math.Ceiling(unionBox.Max.Y); y++) {
-                    for (var x = Math.Floor(unionBox.Min.X); x < Math.Ceiling(unionBox.Max.X); x++) {
-                        var boundingBox = new BoundingBox(x, y, z, x + 1, y + 1, z + 1);
-                        var boxBrep = boundingBox.ToBrep();
-                        var meshBox = Mesh.CreateFromBox(boundingBox, 1, 1, 1);
-                        for (var i = 0; i < geometryClean.Count; i++) {
-                            var geometry = geometryClean[i];
-                            var geometryBox = geometryBoxes[i];
-                            if (boundingBox.Contains(geometryBox)) {
-                                outputGeometry.Add(geometry);
-                                continue;
-                            }
-                            if (geometry.HasBrepForm) {
-                                var brep = (Brep)geometry;
-                                var pieces = brep.Trim(boxBrep, Config.EPSILON);
-                                outputGeometry.AddRange(pieces);
-                            }
-                            if (geometry.ObjectType == ObjectType.Mesh) {
-                                var mesh = (Mesh)geometry;
-                                var pieces = mesh.Split(meshBox);
-                                foreach (var piece in pieces) {
-                                    var currentMeshBox = piece.GetBoundingBox(true);
-                                    if (boundingBox.Contains(currentMeshBox.Center)) {
-                                        outputGeometry.Add(piece);
-                                    }
-                                }
-                            }
-                            if (geometry.ObjectType == ObjectType.Curve) {
-                                var curve = (Curve)geometry;
-                                var pieces = curve.Split(boxBrep, Config.EPSILON * 1000, Config.EPSILON * 1000);
-                                foreach (var piece in pieces) {
-                                    var curveBox = piece.GetBoundingBox(true);
-                                    if (boundingBox.Contains(curveBox.Center)) {
-                                        outputGeometry.Add(piece);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+                var plane = Plane.WorldXY;
+                plane.Origin = new Point3d(0, 0, z);
+                var surface = Brep.CreateFromCornerPoints(new Point3d(minX, minY, z),
+                                                          new Point3d(maxX, minY, z),
+                                                          new Point3d(maxX, maxY, z),
+                                                          new Point3d(minX, maxY, z),
+                                                          Config.EPSILON);
+                chop(plane, surface, toBeChopped, ref alreadyChopped, out var nextChopped);
+                toBeChopped = nextChopped;
             }
+
+            toBeChopped = toBeChopped.Concat(alreadyChopped).ToList();
+            alreadyChopped = new List<GeometryBase>();
+
+            for (var y = Math.Floor(unionBox.Min.Y); y < Math.Ceiling(unionBox.Max.Y); y++) {
+                var plane = Plane.WorldZX;
+                plane.Origin = new Point3d(0, y, 0);
+                var surface = Brep.CreateFromCornerPoints(new Point3d(minX, y, minZ),
+                                                          new Point3d(maxX, y, minZ),
+                                                          new Point3d(maxX, y, maxZ),
+                                                          new Point3d(minX, y, maxZ),
+                                                          Config.EPSILON);
+                chop(plane, surface, toBeChopped, ref alreadyChopped, out var nextChopped);
+                toBeChopped = nextChopped;
+            }
+
+            toBeChopped = toBeChopped.Concat(alreadyChopped).ToList();
+            alreadyChopped = new List<GeometryBase>();
+
+            for (var x = Math.Floor(unionBox.Min.X); x < Math.Ceiling(unionBox.Max.X); x++) {
+                var plane = Plane.WorldYZ;
+                plane.Origin = new Point3d(x, 0, 0);
+
+                var surface = Brep.CreateFromCornerPoints(new Point3d(x, minY, minZ),
+                                                          new Point3d(x, maxY, minZ),
+                                                          new Point3d(x, maxY, maxZ),
+                                                          new Point3d(x, minY, maxZ),
+                                                          Config.EPSILON);
+                chop(plane, surface, toBeChopped, ref alreadyChopped, out var nextChopped);
+                toBeChopped = nextChopped;
+            }
+
+            var outputGeometry = toBeChopped.Concat(alreadyChopped);
 
             DA.SetDataList(0, outputGeometry);
         }
 
+        private static void chop(Plane plane, Brep surface, IEnumerable<GeometryBase> toBeChopped, ref List<GeometryBase> alreadyChopped, out List<GeometryBase> nextChopped) {
+            var pieces = new List<GeometryBase>();
+            foreach (var geometry in toBeChopped) {
+                if (geometry.HasBrepForm) {
+                    var brep = (Brep)geometry;
+                    var cutouts = brep.Split(surface, Config.EPSILON);
+                    if (cutouts.Any()) {
+                        pieces.AddRange(cutouts);
+                    } else {
+                        pieces.Add(brep);
+                    }
+                }
+                if (geometry.ObjectType == ObjectType.Curve) {
+                    var curve = (Curve)geometry;
+                    var intersection = Intersection.CurvePlane(curve, plane, Config.EPSILON);
+                    if (intersection != null) {
+                        var splittingParams = intersection
+                            .Where(intersect => intersect.IsPoint || intersect.IsOverlap)
+                            .Select(intersect => intersect.ParameterA);
+                        pieces.AddRange(curve.Split(splittingParams));
+                    } else {
+                        pieces.Add(curve);
+                    }
+                }
+            }
+            nextChopped = new List<GeometryBase>();
+            foreach (var piece in pieces) {
+                var meshBoundingBox = piece.GetBoundingBox(true);
+                var distanceToPlane = plane.DistanceTo(meshBoundingBox.Center);
+                if (distanceToPlane > 0) {
+                    nextChopped.Add(piece);
+                } else {
+                    alreadyChopped.Add(piece);
+                }
+            }
+        }
 
         /// <summary>
         /// The Exposure property controls where in the panel a component icon
